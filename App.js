c40d5de97616c6bea36e55ce9ea2e9bafb7243e1
Pii <pii@Piis-iMac.local>
2019-10-20 23:00:49 -0300

    Recebendo o calculo

--- App.js
+++ App.js
@@ -1,16 +1,62 @@
 import React, { Component } from 'react';
 import {
   StyleSheet,
-  View,
   Text,
+  TextInput,
+  TouchableOpacity,
+  View,
 } from 'react-native';
+
 
 type Props = {};
 export default class App extends Component<Props> {
+
+  constructor(props) {
+    super(props)
+    this.state = { altura: 0, massa: 0, resultado: 0, resultadoText: "" }
+    this.calcular = this.calcular.bind(this)
+  }
+
+  calcular() {
+    let imc = this.state.massa / (this.state.altura * this.state.altura)
+    let s = this.state
+    s.resultado = imc
+
+    if (s.resultado === 0) {
+      s.resultado = "Olá"
+    }
+
+    if (s.resultado < 16) {
+      s.resultadoText = "Magreza Grave"
+    } else if (s.resultado < 17) {
+      s.resultadoText = "Magreza Moderada"
+    } else if (s.resultado < 18.5) {
+      s.resultadoText = "Magreza Leve"
+    } else if (s.resultado < 25) {
+      s.resultadoText = "Saudável"
+    } else if (s.resultado < 30) {
+      s.resultadoText = "Sobrepeso"
+    } else if (s.resultado < 35) {
+      s.resultadoText = "Obesidade Grau I"
+    } else if (s.resultado < 40) {
+      s.resultadoText = "Obesidade Grau II"
+    } else {
+      s.resultadoText = "Obesidade Grau III"
+    }
+
+    this.setState(s)
+  }
+
   render() {
     return (
       <View style={styles.container}>
-
+        <View style={styles.entradas}>
+          <TextInput placeholder="Massa" keyboardType="numeric" style={styles.input} onChangeText={(massa) => { this.setState({ massa }) }} ></TextInput>
+          <TextInput placeholder="Altura" keyboardType="numeric" style={styles.input} onChangeText={(altura) => { this.setState({ altura }) }} ></TextInput>
+        </View>
+        <Text style={styles.resultado}>{this.state.resultado.toFixed(2)}</Text>
+        <Text style={[styles.resultado, { fontSize: 35 }]}>{this.state.resultadoText}</Text>
+        <TouchableOpacity onPress={this.calcular} style={styles.button}><Text style={styles.buttonText}>Calcule Já!!!</Text></TouchableOpacity>
       </View>
     );
   }
@@ -19,8 +65,38 @@ export default class App extends Component<Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
     backgroundColor: "#f5fcff",
+  },
+  entradas: {
+    flexDirection: "row",
+  },
+  input: {
+    height: 80,
+    textAlign: "center",
+    width: "50%",
+    fontSize: 50,
+    marginTop: 80,
+  },
+  button: {
+    marginTop: 100,
+    backgroundColor: "#ff0000",
+    width: "80%",
+    alignSelf: "center",
+    borderRadius: 20,
+  },
+  buttonText: {
+    alignSelf: "center",
+    padding: 30,
+    fontSize: 25,
+    color: "#fff",
+    fontWeight: "bold",
+    textTransform: "uppercase",
+  },
+  resultado: {
+    marginTop: 50,
+    alignSelf: "center",
+    color: "#ccc",
+    fontSize: 65,
+    padding: 15,
   }
 });
